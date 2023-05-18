a7edde330b8dfd68228a95012347205e0fdeb44a
MayfieldRE <133822664+MayfieldRE@users.noreply.github.com>
2023-05-18 13:40:53 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='mre-dependencies',
+    version='0.0.1',
+    author='Zach Snyder',
+    author_email='zach@mayfield.energy',
+    description='Python dependencies for Mayfield Energy Excel automations',
+    long_description="",
+    long_description_content_type="text/markdown",
+    url='https://github.com/MayfieldRE/dependencies.git',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+    },
+    packages=['mre-dependencies'],
+    install_requires=[
+    	'xlwings',
+      'openpyxl',
+    	'numpy',
+    	'matplotlib',
+    	'pandas'
+    ]
+)
