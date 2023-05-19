04ce783d5c7b2e6d7dec31ca98f9d5d40deced18
zachsnyder1 <zachsnyder3@gmail.com>
2023-05-19 10:43:35 -0700

    v 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mre-dependencies',
-    version='0.0.1',
+    version='0.0.2',
     author='Zach Snyder',
     author_email='zach@mayfield.energy',
     description='Python dependencies for Mayfield Energy Excel automations',
